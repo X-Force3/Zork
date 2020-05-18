5c9f036abcdc095d8c1635421d2035617f4cb000
Luz <luz_laura_perez_nina@hotmail.com>
2020-05-17 22:42:28 -0300

    Lugar tests
    Método describirObjetosDisponibles() con sus respectivos tests de acuerdo a las cantidad de items.

--- src/entidades/Lugar.java
+++ src/entidades/Lugar.java
@@ -7,20 +7,63 @@ public class Lugar {
 	private String nombre;
 	private Genero genero;
 	private Numero numero;
-	private List<String> items;
+	private List<Item> items;
 	
-	public Lugar() {
-
+	public Lugar(String nombre, Genero genero, Numero numero, List<Item> items) {
+		super();
+		this.nombre = nombre;
+		this.genero = genero;
+		this.numero = numero;
+		this.items = items;
+	}
+	
+	public String describirObjetosDisponibles() {
+		String queHay = "";
+		if(items.isEmpty())
+			queHay = "no hay nada. Prueba en otro lugar";
+		else {
+			if (items.size() == 1) {
+				queHay = "hay " + conjugarItem(this.items.get(0));
+			}else {
+				for(int i = 0; i < items.size() - 2; i++) {
+					Item item = this.getItems().get(i);
+					queHay = queHay + conjugarItem(item) + ", ";
+				}
+				Item anteultimoItem = this.items.get(items.size()-2);
+				Item ultimoItem = this.items.get(items.size()-1);
+				queHay = "hay " + queHay + conjugarItem(anteultimoItem) + " y " + conjugarItem(ultimoItem); 
+			}
+		}
+		return "En " + conjugar(genero, numero) + " " + queHay + ".";
 	}
 
-	/// Se recorre la collection
-	public void describirObjetosDisponibles() {
+	private String conjugar(Genero genero, Numero numero) {
+		String conjugacion= "";
+		if(this.genero == Genero.FEMALE) {
+			conjugacion = this.numero == Numero.SINGULAR? "la" : "las";
+		}else {
+			conjugacion = this.numero == Numero.SINGULAR? "el" : "los";
+		}
+		return conjugacion + " " + this.nombre;
+	}
+	
+	private String conjugarItem(Item item) {
+		String conjugacion= "";
+		if(item.getGenero() == Genero.FEMALE) {
+			conjugacion = item.getNumero() == Numero.SINGULAR? "una " : "";
+		}else {
+			conjugacion = item.getNumero() == Numero.SINGULAR? "un " : "";
+		}
+		return conjugacion + item.getNombre();
+	}
 
+	public List<Item> getItems() {
+		return items;
 	}
 
 	@Override
 	public String toString() {
 		return "Lugar [nombre=" + nombre + ", genero=" + genero + ", numero=" + numero + ", items=" + items + "]";
 	}
-
+	
 }
--- test/entidades/LugarTest.java
+++ test/entidades/LugarTest.java
@@ -1,5 +1,9 @@
 package entidades;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,7 +13,38 @@ public class LugarTest {
 
 	@Before
 	public void setup() {
+		ArrayList<Item> items = new ArrayList<Item>();
+		lugar = new Lugar("muelle", Genero.MALE, Numero.SINGULAR, items);
+	}
 
+	@Test
+	public void queDescribaElLugarSinItems() {
+		System.out.println(lugar.describirObjetosDisponibles());
+		Assert.assertEquals("En el muelle no hay nada. Prueba en otro lugar.", lugar.describirObjetosDisponibles());
+	}
+
+	@Test
+	public void queDescribaElLugarConUnItem() {
+		lugar.getItems().add(new Item("espejo",Genero.MALE, Numero.SINGULAR, Arrays.asList("usar"),Arrays.asList("npc")));
+		System.out.println(lugar.describirObjetosDisponibles());
+		Assert.assertEquals("En el muelle hay un espejo.", lugar.describirObjetosDisponibles());
+	}
+	
+	@Test
+	public void queDescribaElLugarConDosItems() {
+		lugar.getItems().add(new Item("piedras",Genero.FEMALE, Numero.PLURAL, Arrays.asList("usar"),Arrays.asList("npc")));
+		lugar.getItems().add(new Item("linterna",Genero.FEMALE, Numero.SINGULAR, Arrays.asList("usar"),Arrays.asList("npc")));
+		System.out.println(lugar.describirObjetosDisponibles());
+		Assert.assertEquals("En el muelle hay piedras y una linterna.", lugar.describirObjetosDisponibles());
+	}
+
+	@Test
+	public void queDescribaElLugarConMuchosItems() {
+		lugar.getItems().add(new Item("espejo",Genero.MALE, Numero.SINGULAR, Arrays.asList("usar"),Arrays.asList("npc")));
+		lugar.getItems().add(new Item("martillo",Genero.MALE, Numero.SINGULAR, Arrays.asList("usar"),Arrays.asList("npc")));
+		lugar.getItems().add(new Item("linterna",Genero.FEMALE, Numero.SINGULAR, Arrays.asList("usar"),Arrays.asList("npc")));
+		System.out.println(lugar.describirObjetosDisponibles());
+		Assert.assertEquals("En el muelle hay un espejo, un martillo y una linterna.", lugar.describirObjetosDisponibles());
 	}
 
 	/// Se probara que un objeto disponible en el place, no siga disponible una vez
