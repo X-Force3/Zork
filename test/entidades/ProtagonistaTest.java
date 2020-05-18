9c7b54f8956088a670716840b298b340c3405bca
Nahuel <nahulmz98@gmail.com>
2020-05-18 15:54:01 -0300

    Tests
    test añadirItem

--- test/entidades/ProtagonistaTest.java
+++ test/entidades/ProtagonistaTest.java
@@ -1,20 +1,35 @@
 package entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ProtagonistaTest {
 
-	Protagonista protagonista;
+	Protagonista protagonista = new Protagonista("Nahuel");
 
 	@Before
 	public void setup() {
-
+		
 	}
 
 	@Test
 	public void queRecogeCorrectamenteUnItem() {
-
+		
+		List<String> acciones = new ArrayList<String>();
+		List<String> efectosSobre = new ArrayList<String>();
+		acciones.add("Usar");
+		efectosSobre.add("Asustarse");
+		
+		Item item = new Item("Espejo",Genero.MALE,Numero.SINGULAR,acciones, efectosSobre);
+			
+			protagonista.añadirItem(item);
+			
+			Assert.assertEquals(false, protagonista.añadirItem(item));///si devuelve falso, quiere decir que ya lo tiene en el inventario
+			
 	}
 
 	@Test
