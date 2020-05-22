84564ed113b2680c9cef96c391f82e64abb7d862
bcoronel8 <bcoronel8@hotmail.com>
2020-05-22 20:57:05 -0300

    ItemTest fix
    .

--- test/entidades/ItemTest.java
+++ test/entidades/ItemTest.java
@@ -9,8 +9,6 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-
-
 public class ItemTest {
 
 	Item item;
@@ -23,50 +21,49 @@ public class ItemTest {
 	public void setup() {
 		acciones = new ArrayList<String>();
 		efectosSobre = new ArrayList<String>();
-		
+
 		acciones.add("Usar");
 		efectosSobre.add("Thanos");
 		efectosSobre.add("Suelo");
 		efectosSobre.add("Clavo");
 		item = new Item("Martillo Thor", Genero.MALE, Numero.SINGULAR, acciones, efectosSobre);
-		
+
 	}
 
 	@Test
 	public void seUtilizaCorrectamenteSobreNpc() {
-		npc = new Npc("Thanos", Genero.MALE, Numero.SINGULAR, "Thanos, monstruo malvado", "Hola, soy Thanos", null);
-		assertTrue(item.realizarAccion(npc,"Usar"));
+		npc = new Npc("Thanos", Genero.MALE, Numero.SINGULAR, "Thanos, monstruo malvado", "Hola, soy Thanos", null,
+				false);
+		assertTrue(item.realizarAccion(npc, "Usar"));
 	}
 
-	
 	@Test
 	public void seIntentaUtilizarEnNpcIncorrecto() {
-		npc = new Npc("Messi", Genero.MALE, Numero.SINGULAR, "Messi, futbolista", "Hola, soy messi", null);
-		assertFalse(item.realizarAccion(npc,"Usar"));
+		npc = new Npc("Messi", Genero.MALE, Numero.SINGULAR, "Messi, futbolista", "Hola, soy messi", null, false);
+		assertFalse(item.realizarAccion(npc, "Usar"));
 	}
-	
+
 	@Test
-	public void  seIntentaUtilizarEnNpcCorrectoPeroConAccionIncorrecta() {
-		npc = new Npc("Roberta", Genero.FEMALE, Numero.SINGULAR, "Roberta,   ", "Hola, soy Roberta", null);
-		assertFalse(item.realizarAccion(npc,"Derretir"));
+	public void seIntentaUtilizarEnNpcCorrectoPeroConAccionIncorrecta() {
+		npc = new Npc("Roberta", Genero.FEMALE, Numero.SINGULAR, "Roberta,   ", "Hola, soy Roberta", null, false);
+		assertFalse(item.realizarAccion(npc, "Derretir"));
 	}
-	
+
 	@Test
 	public void seUtilizaCorrectamenteSobreLugar() {
 		lugar = new Lugar("Suelo", Genero.MALE, Numero.SINGULAR, null);
-		assertTrue(item.realizarAccion(lugar,"Usar"));
+		assertTrue(item.realizarAccion(lugar, "Usar"));
 	}
 
-	
 	@Test
 	public void seIntentaUtilizarEnLugarIncorrecto() {
 		lugar = new Lugar("Mesa", Genero.FEMALE, Numero.SINGULAR, null);
-		assertFalse(item.realizarAccion(lugar,"Usar"));
+		assertFalse(item.realizarAccion(lugar, "Usar"));
 	}
-	
+
 	@Test
-	public void  seIntentaUtilizarEnLugarCorrectoPeroConAccionIncorrecta() {
+	public void seIntentaUtilizarEnLugarCorrectoPeroConAccionIncorrecta() {
 		lugar = new Lugar("Suelo", Genero.MALE, Numero.SINGULAR, null);
-		assertFalse(item.realizarAccion(lugar,"Derretir"));
+		assertFalse(item.realizarAccion(lugar, "Derretir"));
 	}
 }
