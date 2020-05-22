6b6403bfa0045c6378524f8dd7d80bcc910a88b0
bcoronel8 <bcoronel8@hotmail.com>
2020-05-22 07:34:35 -0300

    Se modifica la Class Test y ConexionTest
    .-

--- src/entidades/Conexion.java
+++ src/entidades/Conexion.java
@@ -1,39 +1,43 @@
 package entidades;
 
-import java.util.List;
-
 public class Conexion {
 
 	private Direccion direccion;
-	private String locacionDestino;
-	private String obstaculo;
-	
+	private Ubicacion ubicacionDestino;
+	private Npc obstaculo;
+
 	/// Podria haber un ID para asi diferenciar los puntos cardinales de una misma o
 	/// distinta ubicacion.
 
-	public Conexion(Direccion direccion, String locacionDestino, String obstaculo) {
+	public Conexion(Direccion direccion, Ubicacion locacionDestino, Npc obstaculo) {
 		super();
 		this.direccion = direccion;
-		this.locacionDestino = locacionDestino;
+		this.ubicacionDestino = locacionDestino;
 		this.obstaculo = obstaculo;
 	}
-	
+
 	public Direccion getDireccion() {
 		return direccion;
 	}
 
-	public String getLocacionDestino() {
-		return locacionDestino;
+	public Ubicacion getUbicacionDestino() {
+		return ubicacionDestino;
 	}
 
-	public String getObstaculo() {
+	public Npc getObstaculo() {
 		return obstaculo;
 	}
 
 	@Override
 	public String toString() {
-		return "Conexion [direccion=" + direccion + ", locacionDestino=" + locacionDestino + ", obstaculo=" + obstaculo
-				+ "]";
+		return "direccion :" + " locacionDestino ";
+	}
+
+	public Ubicacion irHaciaDestino() {
+		if (obstaculo.getCondicionDeObstaculo()) {
+			return this.ubicacionDestino;
+		} else
+			return null;
 	}
 
 }
--- test/entidades/ConexionTest.java
+++ test/entidades/ConexionTest.java
@@ -1,20 +1,51 @@
 package entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ConexionTest {
 
 	Conexion conexion;
+	Npc npc;
+	Ubicacion ubicacionInicial;
+	Ubicacion ubicacionDestino;
 
 	@Before
 	public void setup() {
+		npc = new Npc("Saku", Genero.FEMALE, Numero.SINGULAR, "Una señorita con un tapado rojo", " ", null, false);
+		ubicacionDestino = new Ubicacion("Tienda electronica", Genero.MALE, Numero.SINGULAR, null, null, null);
+		conexion = new Conexion(Direccion.NORTE, ubicacionDestino, npc);
+
+		List<Npc> listaNpc = new ArrayList<Npc>();
+		listaNpc.add(npc);
+		List<Conexion> listaConexiones = new ArrayList<Conexion>();
+		listaConexiones.add(conexion);
+		ubicacionInicial = new Ubicacion("Bar cyberpunk", Genero.MALE, Numero.SINGULAR, null, listaNpc,
+				listaConexiones);
 
 	}
 
 	@Test
 	public void queConectaCorrectamenteHaciaOtraUbicacion() {
 
+		Assert.assertEquals(conexion.getUbicacionDestino(), ubicacionDestino);
+
+	}
+
+	@Test
+	public void queEsUnaConexionDeUnaUbicacionOriginaria() {
+
+		Assert.assertTrue(ubicacionInicial.getConexiones().contains(conexion));
+	}
+
+	@Test
+	public void queSeDesplazaCorrectamenteSegunObstaculo() {
+		/// El Npc esta obstaculizando la conexion
+		Assert.assertEquals(conexion.irHaciaDestino(), null);
 	}
 
 }
